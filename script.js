f46b37d6f25ca17c007b408155ca801248e3a92f
Borja Ares <76764937+BorjaAres@users.noreply.github.com>
2023-07-01 13:35:03 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-// Prompt the user to enter a number and parse it as a floating-point value
-let num1 = parseFloat(prompt("Give me a number", "2"));
+// Prompt the user to enter three numbers and parse them as a floating-point value
+let num1 = parseFloat(prompt("Give me a number"));
+let num2 = parseFloat(prompt("And another one!"));
+let num3 = parseFloat(prompt("Last one"));
 
-// Prompt the user to enter another number and parse it as a floating-point value
-let num2 = parseFloat(prompt("And another one!", "3"));
-
-// Compare the two numbers
-if (num1 > num2) {
-  // If num1 is greater, display the result
-  document.getElementById('myDiv1').innerHTML = num1 + " is greater than " + num2;
-} else if (num1 < num2) {
-  // If num2 is greater, display the result
-  document.getElementById('myDiv1').innerHTML = num2 + " is greater than " + num1;
+// Compare the numbers and display the result
+if (num1 > num2 && num1 > num3) {
+    document.getElementById('myDiv1').innerHTML = num1 + " is greater than " + num2 + " and " + num3;
+} else if (num2 > num1 && num2 > num3) {
+    document.getElementById('myDiv1').innerHTML = num2 + " is greater than " + num1 + " and " + num3;
+} else if (num3 > num1 && num3 > num2) {
+    document.getElementById('myDiv1').innerHTML = num3 + " is greater than " + num1 + " and " + num2;
 } else {
-  // If both numbers are equal, display the result
-  document.getElementById('myDiv1').innerHTML = "Both numbers are equal";
+    document.getElementById('myDiv1').innerHTML = "The numbers are equal";
 }
